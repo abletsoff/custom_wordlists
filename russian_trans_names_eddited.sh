7033fe55d056a5d5eafc95cf3bb3acc5782feb6a
Andrew Abletsov <abletsoff@gmail.com>
2024-10-06 12:25:36 +0300

    adjusted

--- russian_trans_names_eddited.sh
+++ russian_trans_names_eddited.sh
@@ -1,9 +1,18 @@
+filename="russian_trans_names_eddited.txt"
+
 readarray -t names < russian_trans_names.txt
 for name in ${names[@]}; do 
     if [[ $(echo "$name" | grep -P 'j$') != '' ]]; then
-        echo "$name" | sed 's/j$/y/g'
-        echo "$name" | sed 's/j$/i/g'
-    else
-        echo "$name"
+        if [[ $(echo "$name" | grep -P 'ij$') != '' ]]; then
+            echo "$name" | sed 's/j$//g' >> "$filename.tmp"
+        fi
+
+        echo "$name" | sed 's/j$/y/g' >> "$filename.tmp"
+        echo "$name" | sed 's/j$/i/g' >> "$filename.tmp"
+    else 
+        echo "$name" >> "$filename.tmp"
     fi
 done
+
+perl -ne 'print unless $seen{$_}++' "$filename.tmp" > "$filename"
+rm "$filename.tmp"
